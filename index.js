ee96a0391ab69a09bd94d23b957980569c5b5c7d
Neechelle James <neechelle.james@gmail.com>
2023-03-28 15:48:16 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+main.remove();
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent = 'Neechelle is the champion';
+element.append('h1');
 // Write your code here!
